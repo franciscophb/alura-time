12a3801ac74e1dd7a330f7c330caadfb92401cb7
Francisco Araújo <franciscophb@hotmail.com>
2025-09-24 18:46:33 -0300

    adicionado a função de troca de imagens do botão play e stop

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,8 +1,19 @@
 const { ipcRenderer } = require('electron');
 
 let linkSobre = document.querySelector('#link-sobre');
+let botaoPlay = document.querySelector('.botao-play');
 
 
 linkSobre.addEventListener('click' , function(){
     ipcRenderer.send('abrir-janela-sobre');
+});
+
+let imgs = ['img/play-button.svg', 'img/stop-button.svg']
+botaoPlay.addEventListener('click', function functionName(){
+    //comando console é apenas para teste
+    //console.log('Pre inversao', imgs);
+    imgs = imgs.reverse();
+    //console.log('POs inversao', imgs);
+    botaoPlay.src = imgs[0];
+
 });
